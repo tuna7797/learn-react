3a58b9f090de6d145c0b2317b75fab01ebcd76b4
tuna7797 <gmlwjd7731@gmail.com>
2023-08-11 10:36:07 +0900

    컴포넌트 분리/관리 하기

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Description from "./components/Description";
+import Heading from "./components/Heading";
+import RenderingProcessList from "./components/RenderingProcessList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Description />
+      <RenderingProcessList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+
+
+function Heading() {
+  return <h1>리액트</h1>;
+}
+
+export default Heading;
