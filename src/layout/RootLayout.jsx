45c27b4a924f2d688e4e5c7009cd2c796be47d81
tuna7797 <gmlwjd7731@gmail.com>
2023-08-11 15:49:37 +0900

    페이지 컴포넌트

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
 
 import FooterBar from './FooterBar';
 import HeaderBar from './HeaderBar';
 
 function RootLayout(props) {
-  console.log(props);
+  
   return (
     <div>
       <HeaderBar />
-      <main></main>
+      <main>
+        {props.children}
+      </main>
       <FooterBar />
     </div>
   )
