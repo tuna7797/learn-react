b9b7bea9142c81ef25acfb3bbb30e8a3cd7791b3
tuna7797 <gmlwjd7731@gmail.com>
2023-08-15 02:07:51 +0900

    Vite 절대 경로 구성 - resolve.alias - rollup alias plugin - Node.js path module

--- src/pages/Practice/Practice.jsx
+++ src/pages/Practice/Practice.jsx
@@ -1,15 +1,18 @@
 // 스타일 및 에셋 연결
-import '../styles/Practice.css'
+import './Practice.css';
+import { getRandom } from '@/utils/getRandomMinMax';
 
-const imageType = 'react';
+/* 데이터 ---------------------------------------------------------------------- */
 
-const isShowReactImage = true;
+let imageType = 'react'; // 'react' | 'vite'
+
+let isShowReactImage = true;
 
 const statusMessage = [
-  '⌛️ 대기',
-  '⏳ 로딩 중...',
-  '✅ 로딩 성공!',
-  '❌ 로딩 실패.',
+  '⌛️ 대기', // pending
+  '⏳ 로딩 중...', // loading
+  '✅ 로딩 성공!', // success (resolve)
+  '❌ 로딩 실패.', // error (fail, reject)
 ];
 
 const reactLibrary = {
@@ -20,6 +23,8 @@ const reactLibrary = {
   license: 'MIT',
 };
 
+/* -------------------------------------------------------------------------- */
+
 function Practice() {
   const renderList = () => {
     return null;
@@ -27,14 +32,33 @@ function Practice() {
 
   return (
     <div className="Practice">
-      <h1>JSX 인 액션</h1>
+      <h2>JSX 인 액션</h2>
       <hr />
       <dl className="descriptionList">
         <dt>데이터 바인딩(data binding)</dt>
         <dd>
           <p>상태 메시지(status message)를 연결해 화면에 출력합니다.</p>
           <span className="status">
-            {/* statusMessage 값을 화면에 표시합니다. (랜덤 표시도 도전!) */}
+            {/* displaying data */}
+
+            {/* 1-1. statusMessage 배열의 원소 중 하나의 값을 화면에 표시합니다. */}
+            {/* { statusMessage } */}
+            {/* { statusMessage[0] } */}
+            {/* { statusMessage[1] } */}
+            {/* { statusMessage[2] } */}
+            {/* { statusMessage[3] } */}
+            {/* { statusMessage[4] } */}
+
+            {/* [🚨 오류] 객체 타입은 React의 자식 타입으로 적절하지 않습니다. */}
+            {/* { reactLibrary } */}
+
+            {/* 1-2. 랜덤 숫자 인덱스의 원소 값 표시 도전! */}
+
+            {/* JSX는 JavaScript 표현식(Expression) 그러므로 항상 결과 값을 반환 */}
+            {/* JSX 안에서는 문을 사용할 수 없다. (❌ if, for, while, switch ..) */}
+            {/* {statusMessage[Math.floor(Math.random() * statusMessage.length)]} */}
+            {/* 유틸리티 함수를 사용하면 클린 코드 작성 가능 */}
+            {statusMessage[getRandom(statusMessage.length)]}
           </span>
         </dd>
         <dt>조건부 렌더링(conditional rendering)</dt>
