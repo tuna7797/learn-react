91c8281d98ac9a1f2293a9d0618a37152e799112
tuna7797 <gmlwjd7731@gmail.com>
2023-08-15 23:19:04 +0900

    조건부 렌더링 완료 - 문 또는 식 - 함수 몸체(문, 식 모두 사용 가능) - JSX 내부(식만 사용 가능, 함수 실행도 가능)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
