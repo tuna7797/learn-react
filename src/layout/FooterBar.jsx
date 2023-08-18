e67ddda6a17cf73be6e5eb0da8aded402bb7212d
tuna7797 <gmlwjd7731@gmail.com>
2023-08-18 11:36:56 +0900

    CSS 모듈 해설 - 사용하는 이유 - 작동 원리

--- src/layout/FooterBar.jsx
+++ src/layout/FooterBar.jsx
@@ -1,11 +1,12 @@
-
+import styles from '@/styles/FooterBar.module.css';
 
 function FooterBar() {
   return (
-    <footer>
+    // <footer className='container'>
+    <footer className={styles.container}>
       <small>모든 저작권은 --에 있습니다.</small>
     </footer>
-  )
+  );
 }
 
 export default FooterBar;
