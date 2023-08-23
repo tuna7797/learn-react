383ae5284f112b08f09da99fc17975658bed02e0
tuna7797 <gmlwjd7731@gmail.com>
2023-08-23 22:07:29 +0900

    폼 필드의 value, defaultValue, readOnly, onChange

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useId, useState } from 'react';
 
 // const getOne = () => 1;
 
@@ -12,22 +12,40 @@ function LearnStateAndEffects() {
   // console.log(count1);
   // console.log(count2);
   const [count, setCount] = useState(100);
-  const [step] = useState(12);
+  let [step, setStep] = useState(12);
 
   // 상태 정의와 상태 변경
+
+  // 관리가 까다로운 ID 속성 값을 자동 생성하는 훅
+  const stepperId = useId();
 
   return (
     <div className="m-10 flex flex-col gap-2 items-start" lang="en">
       <h2 className="text-indigo-600 text-2xl uppercase">
         Learn State And Effects {count}
       </h2>
-      <button
-        type="button"
-        onClick={() => setCount(count + step)}
-        className="py-0.5 px-2.5 border rounded-md border-slate-600"
-      >
-        +{step}
-      </button>
+      <div className='flex flex-col gap-2 items-end'>
+        <div className='flex gap-2 items-center'>
+          <label htmlFor={stepperId} className='text-base'>step</label>
+          <input
+            id={stepperId}
+            type="number"
+            value={step}
+            onChange={(e) => {
+              const nextStep = Number(e.target.value);
+              setStep(nextStep);
+            }}
+            className="w-[60px] py-1 px-2 border-slate-300 border rounded-xl"
+          />
+        </div>
+        <button
+          type="button"
+          onClick={() => setCount(count + step)}
+          className="py-0.5 px-2.5 border rounded-md border-slate-600"
+        >
+          +{step}
+        </button>
+      </div>
     </div>
   );
 }
